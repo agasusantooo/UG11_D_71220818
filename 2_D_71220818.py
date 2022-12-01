463d2a68a06ab4e1cedcce27f9da1c4d2df6cf7a
agasusantooo <117900754+agasusantooo@users.noreply.github.com>
2022-12-01 16:14:35 +0700

    Add files via upload

---
+++ 2_D_71220818.py
@@ -0,0 +1,9 @@
+def cetakHuruf () :
+    kata=list(input("Masukkan kata :"))
+    len(kata)
+    if(len(kata)%2==0) :
+        print("Huruf paling ujung pada kata Valorant adalah ant")
+    else :
+        print("Huruf paling ujung pada kata Jerapah adalah jer")
+    return (kata)
+cetakHuruf()
